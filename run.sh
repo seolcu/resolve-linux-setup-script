62ca832ac6914e83d4f48c11d3ccd8075b733119
seolcu <seolcu>
2023-08-10 17:25:30 +0900

    fixed typo

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 distrobox create -n resolve -i fedora:37 -Y
 distrobox enter resolve -- sudo dnf install -y fuse-libs alsa-lib apr apr-util fontconfig freetype libglvnd libglvnd-egl libglvnd-glx libglvnd-opengl libICE librsvg2 libSM libX11 libXcursor libXext libXfixes libXi libXinerama libxkbcommon libxkbcommon-x11 libXrandr libXrender libXtst libXxf86vm mesa-libGLU mtdev pulseaudio-libs xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm mesa-libOpenCL libxcrypt-compat alsa-plugins-pulseaudio
-sudo ./DaVinci_Resolve_linux.run
+sudo ./DaVinci_Resolve_Linux.run
 sudo mkdir /opt/resolve
 distrobox enter resolve -- distrobox-export --app /opt/resolve/bin/resolve
