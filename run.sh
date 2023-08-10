6c4b56fdd23e0047f279c14cb8fd788a31db9ec7
seolcu <seolcu>
2023-08-10 17:42:50 +0900

    mkdir for root

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 distrobox create -n resolve -i fedora:37 -Y
 distrobox enter resolve -- sudo dnf install -y fuse-libs alsa-lib apr apr-util fontconfig freetype libglvnd libglvnd-egl libglvnd-glx libglvnd-opengl libICE librsvg2 libSM libX11 libXcursor libXext libXfixes libXi libXinerama libxkbcommon libxkbcommon-x11 libXrandr libXrender libXtst libXxf86vm mesa-libGLU mtdev pulseaudio-libs xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm mesa-libOpenCL libxcrypt-compat alsa-plugins-pulseaudio
-distrobox enter resolve -- sudo mkdir -p /opt/resolve
+sudo mkdir -p /opt/resolve
 distrobox enter resolve -- sudo ./DaVinci_Resolve_Linux.run
 distrobox enter resolve -- distrobox-export --app /opt/resolve/bin/resolve
