3caeac36656e9428b8ad0c26c6e1e56b883fff80
seolcu <seolcu0112@proton.me>
2023-09-04 00:16:05 +0900

    run permission with chmod

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+chmod +x ./DaVinci_Resolve_Linux.run
 distrobox create -n resolve -i fedora:37 -Y
 distrobox enter resolve -- sudo dnf install -y fuse-libs alsa-lib apr apr-util fontconfig freetype libglvnd libglvnd-egl libglvnd-glx libglvnd-opengl libICE librsvg2 libSM libX11 libXcursor libXext libXfixes libXi libXinerama libxkbcommon libxkbcommon-x11 libXrandr libXrender libXtst libXxf86vm mesa-libGLU mtdev pulseaudio-libs xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm libxcrypt-compat alsa-plugins-pulseaudio
 sudo mkdir -p /opt/resolve/graphics
+sudo cp ./assets/DV_Resolve.png /opt/resolve/graphics/DV_Resolve.png
 distrobox enter resolve -- sudo ./DaVinci_Resolve_Linux.run
 distrobox enter resolve -- distrobox-export --app /opt/resolve/bin/resolve
-sudo cp ./assets/DV_Resolve.png /opt/resolve/graphics/DV_Resolve.png
 echo "You still need to install opencl manually to get your GPU detected"
